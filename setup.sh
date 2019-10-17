90316c566027d2e2a8bf314f529652aef70b8d85
Jayanth <jbalacha@usc.edu>
2019-10-16 20:42:47 -0700

    adds pyenv installation and updates tensorflow dependencies

--- setup.sh
+++ setup.sh
@@ -1,5 +1,20 @@
-sudo apt install git
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
+
+#Install pyenv
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
+git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+exec "$SHELL"
+
+pyenv install 3.6.4
+pyenv virtualenv 3.6.4 mlforgames
+pyenv activate mlforgames
+
 git clone https://github.com/csci-599-applied-ml-for-games/Transfer-Learning-Atari-Games.git
-sudo apt-get install -y libsm6 libxext6 python3-pip
-sudo apt-get install libxrender1 libfontconfig1
-pip3 install -r requirements.txt
+sudo apt-get install -y libsm6 libxext6 libxrender1 libfontconfig1
+pip install -r requirements.txt
